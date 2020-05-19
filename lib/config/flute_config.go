d2c90841efed9090a04c54ac5d22c9126eca2226
ish <ish@innogrid.com>
2020-05-19 10:31:59 +0900

    [Hcloud MiddleWare] Flute Config Parser
    * Parser
      - parseMysql
      - parseHTTP
      - parseIpmi
      - parseWOL

---
+++ lib/config/flute_config.go
@@ -0,0 +1,12 @@
+package config
+
+import "github.com/Terry-Mao/goconf"
+
+var configLocation = "/etc/hcc/flute/flute.conf"
+
+type fluteConfig struct {
+	MysqlConfig *goconf.Section
+	HTTPConfig  *goconf.Section
+	IpmiConfig  *goconf.Section
+	WOLConfig   *goconf.Section
+}
