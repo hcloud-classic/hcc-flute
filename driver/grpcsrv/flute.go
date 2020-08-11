7c709f3caa55aaeee229fdff2651992956dc7a04
ish <ish@innogrid.com>
2020-08-11 17:59:59 +0900

    grpcsrv: Leave logs of received requests

--- driver/grpcsrv/flute.go
+++ driver/grpcsrv/flute.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	pb "hcc/flute/action/grpc/rpcflute"
 	"hcc/flute/dao"
+	"hcc/flute/lib/logger"
 )
 
 type fluteServer struct {
@@ -35,6 +36,8 @@ func returnNodeDetail(nodeDetail *pb.NodeDetail) *pb.NodeDetail {
 }
 
 func (s *fluteServer) CreateNode(_ context.Context, in *pb.ReqCreateNode) (*pb.ResCreateNode, error) {
+	logger.Logger.Println("Request received: CreateNode()")
+
 	node, err := dao.CreateNode(in)
 	if err != nil {
 		return nil, err
@@ -44,6 +47,8 @@ func (s *fluteServer) CreateNode(_ context.Context, in *pb.ReqCreateNode) (*pb.R
 }
 
 func (s *fluteServer) GetNode(_ context.Context, in *pb.ReqGetNode) (*pb.ResGetNode, error) {
+	logger.Logger.Println("Request received: GetNode()")
+
 	node, err := dao.ReadNode(in.GetUUID())
 	if err != nil {
 		return nil, err
@@ -53,6 +58,8 @@ func (s *fluteServer) GetNode(_ context.Context, in *pb.ReqGetNode) (*pb.ResGetN
 }
 
 func (s *fluteServer) GetNodeList(_ context.Context, in *pb.ReqGetNodeList) (*pb.ResGetNodeList, error) {
+	logger.Logger.Println("Request received: GetNodeList()")
+
 	nodeList, err := dao.ReadNodeList(in)
 	if err != nil {
 		return nil, err
@@ -62,6 +69,8 @@ func (s *fluteServer) GetNodeList(_ context.Context, in *pb.ReqGetNodeList) (*pb
 }
 
 func (s *fluteServer) GetNodeNum(_ context.Context, _ *pb.Empty) (*pb.ResGetNodeNum, error) {
+	logger.Logger.Println("Request received: GetNodeNum()")
+
 	nodeNum, err := dao.ReadNodeNum()
 	if err != nil {
 		return nil, err
@@ -71,6 +80,8 @@ func (s *fluteServer) GetNodeNum(_ context.Context, _ *pb.Empty) (*pb.ResGetNode
 }
 
 func (s *fluteServer) UpdateNode(_ context.Context, in *pb.ReqUpdateNode) (*pb.ResUpdateNode, error) {
+	logger.Logger.Println("Request received: UpdateNode()")
+
 	updateNode, err := dao.UpdateNode(in)
 	if err != nil {
 		return nil, err
@@ -80,6 +91,8 @@ func (s *fluteServer) UpdateNode(_ context.Context, in *pb.ReqUpdateNode) (*pb.R
 }
 
 func (s *fluteServer) DeleteNode(_ context.Context, in *pb.ReqDeleteNode) (*pb.ResDeleteNode, error) {
+	logger.Logger.Println("Request received: DeleteNode()")
+
 	uuid, err := dao.DeleteNode(in)
 	if err != nil {
 		return nil, err
@@ -89,6 +102,8 @@ func (s *fluteServer) DeleteNode(_ context.Context, in *pb.ReqDeleteNode) (*pb.R
 }
 
 func (s *fluteServer) NodePowerControl(_ context.Context, in *pb.ReqNodePowerControl) (*pb.ResNodePowerControl, error) {
+	logger.Logger.Println("Request received: NodePowerControl()")
+
 	result, err := dao.NodePowerControl(in)
 	if err != nil {
 		return nil, err
@@ -98,6 +113,8 @@ func (s *fluteServer) NodePowerControl(_ context.Context, in *pb.ReqNodePowerCon
 }
 
 func (s *fluteServer) GetNodePowerState(_ context.Context, in *pb.ReqNodePowerState) (*pb.ResNodePowerState, error) {
+	logger.Logger.Println("Request received: GetNodePowerState()")
+
 	result, err := dao.GetNodePowerState(in)
 	if err != nil {
 		return nil, err
@@ -107,6 +124,8 @@ func (s *fluteServer) GetNodePowerState(_ context.Context, in *pb.ReqNodePowerSt
 }
 
 func (s *fluteServer) CreateNodeDetail(_ context.Context, in *pb.ReqCreateNodeDetail) (*pb.ResCreateNodeDetail, error) {
+	logger.Logger.Println("Request received: CreateNodeDetail()")
+
 	nodeDetail, err := dao.CreateNodeDetail(in)
 	if err != nil {
 		return nil, err
@@ -116,6 +135,8 @@ func (s *fluteServer) CreateNodeDetail(_ context.Context, in *pb.ReqCreateNodeDe
 }
 
 func (s *fluteServer) GetNodeDetail(_ context.Context, in *pb.ReqGetNodeDetail) (*pb.ResGetNodeDetail, error) {
+	logger.Logger.Println("Request received: GetNodeDetail()")
+
 	nodeDetail, err := dao.ReadNodeDetail(in.GetNodeUUID())
 	if err != nil {
 		return nil, err
@@ -125,6 +146,8 @@ func (s *fluteServer) GetNodeDetail(_ context.Context, in *pb.ReqGetNodeDetail) 
 }
 
 func (s *fluteServer) DeleteNodeDetail(_ context.Context, in *pb.ReqDeleteNodeDetail) (*pb.ResDeleteNodeDetail, error) {
+	logger.Logger.Println("Request received: DeleteNodeDetail()")
+
 	nodeUUID, err := dao.DeleteNodeDetail(in)
 	if err != nil {
 		return nil, err
