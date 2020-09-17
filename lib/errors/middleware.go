afce13fa25f8974aec64ce9bbc4f7793d42c6100
ish <ish@innogrid.com>
2020-09-17 15:07:05 +0900

    errors: Do not convert HccErrStack to report form

--- lib/errors/middleware.go
+++ lib/errors/middleware.go
@@ -2,10 +2,10 @@ package errors
 
 // ReturnHccError: Get error code and error string and return as HccErrorStack
 func ReturnHccError(errCode uint64, errText string) HccErrorStack {
-	return *NewHccErrorStack(NewHccError(errCode, errText)).ConvertReportForm()
+	return *NewHccErrorStack(NewHccError(errCode, errText))
 }
 
 // ReturnHccEmptyError: Return dummy error as HccErrorStack
 func ReturnHccEmptyError() HccErrorStack {
-	return *NewHccErrorStack().ConvertReportForm()
+	return *NewHccErrorStack()
 }
