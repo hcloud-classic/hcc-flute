d2a2936dca54cbc9465b2ca37554d8ed3020599d
ish <ish@innogrid.com>
2020-11-12 14:42:04 +0900

    [Hcloud MiddleWare] Flute GraphQL Handler
    - Add mutation handler

---
+++ action/graphql/mutationTypes.go
@@ -0,0 +1,163 @@
+package graphql
+
+import (
+	"errors"
+	"github.com/graphql-go/graphql"
+	graphqlType "hcc/flute/action/graphql/type"
+	"hcc/flute/dao"
+	"hcc/flute/lib/logger"
+	"hcc/flute/lib/wol"
+)
+
+var mutationTypes = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"on_node": &graphql.Field{
+			Type:        graphql.String,
+			Description: "On node",
+			Args: graphql.FieldConfigArgument{
+				"mac": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: on_node")
+				mac, macOk := params.Args["mac"].(string)
+				if macOk {
+					err := wol.OnNode(mac)
+					if err != nil {
+						return nil, err
+					}
+
+					return "Send magic packet to \"" + mac + "\"", nil
+				}
+
+				return nil, errors.New("need mac argument")
+			},
+		},
+		// node DB
+		"create_node": &graphql.Field{
+			Type:        graphqlType.NodeType,
+			Description: "Create new node",
+			Args: graphql.FieldConfigArgument{
+				"bmc_mac_addr": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"bmc_ip": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"pxe_mac_addr": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"status": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"cpu_cores": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+				"memory": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+				"description": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"active": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: create_node")
+				return dao.CreateNode(params.Args)
+			},
+		},
+		"update_node": &graphql.Field{
+			Type:        graphqlType.NodeType,
+			Description: "Update node",
+			Args: graphql.FieldConfigArgument{
+				"uuid": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"server_uuid": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"bmc_mac_addr": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"bmc_ip": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"pxe_mac_addr": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"status": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"cpu_cores": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+				"memory": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+				"description": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"active": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: update_node")
+				return dao.UpdateNode(params.Args)
+			},
+		},
+		"delete_node": &graphql.Field{
+			Type:        graphqlType.NodeType,
+			Description: "Delete node by uuid",
+			Args: graphql.FieldConfigArgument{
+				"uuid": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: delete_node")
+				return dao.DeleteNode(params.Args)
+			},
+		},
+		// node_detail DB
+		"create_node_detail": &graphql.Field{
+			Type:        graphqlType.NodeDetailType,
+			Description: "Create new node_detail",
+			Args: graphql.FieldConfigArgument{
+				"node_uuid": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"cpu_model": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"cpu_processors": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+				"cpu_threads": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: create_node_detail")
+				return dao.CreateNodeDetail(params.Args)
+			},
+		},
+		"delete_node_detail": &graphql.Field{
+			Type:        graphqlType.NodeDetailType,
+			Description: "Delete node_detail by node_uuid",
+			Args: graphql.FieldConfigArgument{
+				"node_uuid": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: delete_node_detail")
+				return dao.DeleteNodeDetail(params.Args)
+			},
+		},
+	},
+})
