8283ce00a5f6d431769f623fb5d4f5160663659f
ish <ish@innogrid.com>
2021-01-08 15:43:56 +0900

    errconv: Add comments to each functions

--- action/grpc/errconv/errconv.go
+++ action/grpc/errconv/errconv.go
@@ -5,14 +5,17 @@ import (
 	errg "github.com/hcloud-classic/pb"
 )
 
+// GrpcToHcc : Convert gRPC error code and text to HCCError
 func GrpcToHcc(eg *errg.HccError) *errh.HccError {
 	return errh.NewHccError(eg.GetErrCode(), eg.GetErrText())
 }
 
+// HccToGrpc : Convert HCCError to gRPC error
 func HccToGrpc(eh *errh.HccError) *errg.HccError {
 	return &errg.HccError{ErrCode: eh.Code(), ErrText: eh.Text()}
 }
 
+// GrpcStackToHcc : Convert gRPC error stack to HCCError stack
 func GrpcStackToHcc(esg *[]*errg.HccError) *errh.HccErrorStack {
 	errStack := errh.NewHccErrorStack()
 
@@ -27,6 +30,7 @@ func GrpcStackToHcc(esg *[]*errg.HccError) *errh.HccErrorStack {
 	return errStack
 }
 
+// HccStackToGrpc : Convert HCCError stack to gRPC error stack
 func HccStackToGrpc(esh *errh.HccErrorStack) []*errg.HccError {
 	ges := []*errg.HccError{}
 	for i := 0; i <= esh.Len(); i++ {
