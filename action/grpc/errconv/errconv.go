b90792373e30f5a8cad29db5322baf99ed153128
ish <ish@innogrid.com>
2021-01-13 11:17:13 +0900

    grpc: Update for HccErrStack changes

--- action/grpc/errconv/errconv.go
+++ action/grpc/errconv/errconv.go
@@ -5,37 +5,41 @@ import (
 	errg "github.com/hcloud-classic/pb"
 )
 
-// GrpcToHcc : Convert gRPC error code and text to HCCError
 func GrpcToHcc(eg *errg.HccError) *errh.HccError {
 	return errh.NewHccError(eg.GetErrCode(), eg.GetErrText())
 }
 
-// HccToGrpc : Convert HCCError to gRPC error
 func HccToGrpc(eh *errh.HccError) *errg.HccError {
 	return &errg.HccError{ErrCode: eh.Code(), ErrText: eh.Text()}
 }
 
-// GrpcStackToHcc : Convert gRPC error stack to HCCError stack
-func GrpcStackToHcc(esg *[]*errg.HccError) *errh.HccErrorStack {
+func GrpcStackToHcc(esg *errg.HccErrorStack) *errh.HccErrorStack {
 	errStack := errh.NewHccErrorStack()
 
-	for i, e := range *esg {
-		if i == 0 {
-			continue
-		}
+	if errStack.Version() != esg.GetVersion() {
+		errStack.IsMixed = true
+	} else {
+		errStack.IsMixed = false
+	}
 
-		_ = errStack.Push(errh.NewHccError(e.GetErrCode(), e.GetErrText()))
+	for _, e := range esg.GetErrStack() {
+		errStack.Push(errh.NewHccError(e.GetErrCode(), e.GetErrText()))
 	}
 
 	return errStack
 }
 
-// HccStackToGrpc : Convert HCCError stack to gRPC error stack
-func HccStackToGrpc(esh *errh.HccErrorStack) []*errg.HccError {
-	ges := []*errg.HccError{}
-	for i := 0; i <= esh.Len(); i++ {
-		ge := &errg.HccError{ErrCode: (*esh.Stack())[i].Code(), ErrText: (*esh.Stack())[i].Text()}
-		ges = append(ges, ge)
+func HccStackToGrpc(esh *errh.HccErrorStack) *errg.HccErrorStack {
+	ges := new(errg.HccErrorStack)
+
+	ges.Version = esh.Version()
+	ges.IsMixed = esh.IsMixed
+
+	for i := 0; i < esh.Len(); i++ {
+		eh := esh.Pop()
+		ge := HccToGrpc(eh)
+		ges.ErrStack = append(ges.GetErrStack(), ge)
 	}
+
 	return ges
 }
